f94fa9af1e82497cacd9bbb371471856324cacfd
Paul Morilhat <paul.morilhat@epitech.eu>
2023-03-20 19:08:29 +0100

    Up: Unitary test for my_swap

--- tests/unitary/lib/my/test_my_swap.c
+++ tests/unitary/lib/my/test_my_swap.c
@@ -15,13 +15,3 @@ Test (my_swap_nbr, basic) {
     cr_assert_eq(nbr1, 84);
     cr_assert_eq(nbr2, 42);
 }
-
-Test (my_swap_str, basic) {
-    char *str1 = my_strdup("Hello");
-    char *str2 = my_strdup("Salut");
-    my_swap_str(str1, str2);
-    cr_assert_str_eq(str1, "Salut");
-    cr_assert_str_eq(str2, "Hello");
-    free(str1);
-    free(str2);
-}
